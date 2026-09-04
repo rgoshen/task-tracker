4d7cce367b17a52576f0b3918fbf56664973fccf
Richard Goshen <rick.goshen@gmail.com>
2026-09-04 12:12:36 -0700

    add ruff linter/formatter, priority and status enums

---
+++ src/task_tracker/priority.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+
+
+class Priority(StrEnum):
+    CRITICAL = "CRITICAL"
+    HIGH = "HIGH"
+    MED = "MED"
+    LOW = "LOW"
---
+++ src/task_tracker/status.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+
+class Status(StrEnum):
+    TO_DO = "TO_DO"
+    IN_PROGRESS = "IN_PROGRESS"
+    DONE = "DONE"
